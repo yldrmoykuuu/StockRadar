ea4d9716e3b9588dd46ba7a9974ccf829a675640
Öykü Yıldırım <yldrmoykuuu@gmail.com>
2025-07-25 10:30:43 +0300

    güncelledim

--- stockChecker.py
+++ stockChecker.py
@@ -201,7 +201,7 @@ def check_stock_zara(url):
  
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    
+    options.add_argument("--headless=new")  # yeni headless mod
     #temp_dir = tempfile.mkdtemp()
     temp_dir = tempfile.mkdtemp()
     options.add_argument(f"--user-data-dir={temp_dir}")
