6ed5165da1e566aee64a3c04c53bcc9f9e8eea32
Öykü Yıldırım <yldrmoykuuu@gmail.com>
2025-07-25 13:11:32 +0300

    güncel

--- stockChecker.py
+++ stockChecker.py
@@ -201,7 +201,7 @@ def check_stock_zara(url):
 
     options = Options()
    
-   
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
