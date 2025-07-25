27701213ebc53a9b88a261a876b1b5e74bfd8c5a
Öykü Yıldırım <yldrmoykuuu@gmail.com>
2025-07-25 11:19:36 +0300

    güncel

--- stockChecker.py
+++ stockChecker.py
@@ -199,10 +199,16 @@ def save_product(product):
 def check_stock_zara(url):
     options = Options()
  
+    
+    options.add_argument("--headless")  # Başsız mod
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
     
  
+   
+
   
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
